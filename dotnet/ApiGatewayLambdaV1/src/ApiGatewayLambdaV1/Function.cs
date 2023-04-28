881f11822089def6781f9a1559b8009f2607cee7
Helio Barroco <heliobarroco@gmail.com>
2023-04-28 16:45:58 -0300

    aws lambda tests
    add the local tests about aws lambdas

---
+++ dotnet/ApiGatewayLambdaV1/src/ApiGatewayLambdaV1/Function.cs
@@ -0,0 +1,35 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ApiGatewayLambdaV1;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        return input.ToUpper();
+    }
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public Output ApiHandler(Input input, ILambdaContext context)
+    {
+        return new Output{
+           Result = input.Message.ToUpper()
+        };
+    }
+}
---
+++ dotnet/ApiGatewayLambdaV1/src/ApiGatewayLambdaV1/Output.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGatewayLambdaV1
+{
+    public class Output
+    {
+        public string Result {get; set;}
+    }
+}
